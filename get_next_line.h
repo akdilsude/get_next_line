a61c3220a4cc49a01dbe956930acbfe0da6e25d3
akdilsude <akdilsude@gmail.com>
2024-11-28 11:58:03 +0300

    get_next_line.h aktualisieren

--- get_next_line.h
+++ get_next_line.h
@@ -24,6 +24,10 @@ size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t n);
-
+int	newline(char *str);
+char	*ft_read(char *str, int fd);
+char	*str_line(char *str);
+char	*new_str(char *str);
+char	*get_next_line(int fd);
 
 #endif
